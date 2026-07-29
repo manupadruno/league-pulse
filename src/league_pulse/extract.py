10fa55282dc0a83e295de5b4fdbe7fc11617737b
Manu Padruno <manupadruno@hotmail.es>
2026-07-29 11:33:46 +0200

    feat: add fetch_scorers to extract.py

--- src/league_pulse/extract.py
+++ src/league_pulse/extract.py
@@ -26,15 +26,38 @@ def fetch_matches(competition_code: str, season: int) -> dict:
     Fetches match data for a specific competition from the football-data.org API.
 
     Args:
-        competition_code (str): _description_
+        competition_code (str): The competition code (e.g., 'PL' for Premier League).
         season (int): The season for which to fetch match data.
 
 
     Returns:
-        dict: _description_
+        dict: A dictionary containing the matches (and teams) data
     """
 
     url = f"{BASE_URL}/competitions/{competition_code}/matches"
+    headers = {"X-Auth-Token": API_TOKEN}
+    payload = {"season": season}
+    response = requests.get(url, headers=headers, params=payload)
+
+    response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+
+    return response.json()
+
+
+def fetch_scorers(competition_code: str, season: int) -> dict:
+    """
+    Fetches scorers data for a specific competition from the football-data.org API.
+
+    Args:
+        competition_code (str): The competition code (e.g., 'PL' for Premier League).
+        season (int): The season for which to fetch match data.
+
+
+    Returns:
+        dict: A dictionary containing the scorers (and players) data.
+    """
+
+    url = f"{BASE_URL}/competitions/{competition_code}/scorers"
     headers = {"X-Auth-Token": API_TOKEN}
     payload = {"season": season}
     response = requests.get(url, headers=headers, params=payload)
